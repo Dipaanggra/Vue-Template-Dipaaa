54be7e36dcd0a3712829b473bbc86e963bc1952c
dipaanggra <gadaakuunn@gmail.com>
2024-04-29 09:24:55 +0700

    first commit

---
+++ src/components/Button/SidebarLink.vue
@@ -0,0 +1,12 @@
+<script setup>
+defineProps(['to', 'name'])
+</script>
+<template>
+  <router-link
+    :class="$route.name == name ? 'bg-gray-900 text-white' : 'bg-white text-black hover:bg-gray-100'"
+    class="text-sm py-2 px-5 rounded-lg"
+    :to="to"
+  >
+    {{ name }}
+  </router-link>
+</template>
---
+++ src/views/StateManagement.vue
@@ -0,0 +1,33 @@
+<script setup>
+import ButtonPrimary from '@/components/Button/ButtonPrimary.vue'
+import { useStockStore } from '../stores/stock.store'
+const store = useStockStore()
+</script>
+
+<template>
+  <div>
+    <p class="pb-3">Stock Management With Pinia</p>
+    <div class="border rounded-lg">
+      <table class="w-full table-auto">
+        <!-- table Head -->
+        <tr class="bg--gray-100 border-b">
+          <td class="px-5 py-2 font-semibold">NAME</td>
+          <td class="px-5 py-2 font-semibold">QUANTITY</td>
+          <td class="px-5 py-2 font-semibold ">ACTION</td>
+        </tr>
+        <!-- table Data -->
+        <tbody class="divide-y">
+          <tr class="hover:bg-gray-100"
+            v-for="(stock, index) in store.stock_list" :key="index">
+            <td class="px-5 py-2">{{ stock.name }}</td>
+            <td class="px-5 py-2">{{ stock.qty }}</td>
+            <td class="px-5 py-2">
+              <ButtonPrimary @click="store.decreaseStock(index)" class="mr-2"> - </ButtonPrimary>
+              <ButtonPrimary @click="store.increaseStock(index)"> + </ButtonPrimary>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>

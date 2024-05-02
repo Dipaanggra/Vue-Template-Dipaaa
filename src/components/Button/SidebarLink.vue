eac381c00f0a4184c74f0ab3d396de2e677ad825
dipaanggra <gadaakuunn@gmail.com>
2024-05-02 09:06:27 +0700

    first commit

--- src/components/Button/SidebarLink.vue
+++ src/components/Button/SidebarLink.vue
@@ -3,7 +3,7 @@ defineProps(['to', 'name'])
 </script>
 <template>
   <router-link
-    :class="$route.name == name ? 'bg-gray-900 text-white mb-2 mt-2' : 'bg-white text-black hover:bg-gray-100'"
+    :class="$route.name == name ? 'bg-gray-900 text-white mb-1 mt-1' : 'bg-white mb-1 mt-1 text-black hover:bg-gray-100'"
     class="text-sm py-2 px-5 rounded-lg"
     :to="to"
   >
